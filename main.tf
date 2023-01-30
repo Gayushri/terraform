b6f63c59efc1093f834f446dc1f035eb0ad35ede
root <root@ip-172-31-39-56.ap-south-1.compute.internal>
2023-01-30 11:22:26 +0000

    added

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+    access_key = "AKIAQQBJ7VNGHZKDZUIF"
+    secret_key = "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"
+    region     = "ap-south-1"
+}
+
+resource "aws_instance" "first_instance" {
+    ami           = "ami-0be0a52ed3f231c12"
+    instance_type = "t2.micro"
+        tags = {
+         Name = "server1"
+        }
+
+resource "aws_vpc" "first-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+   Name = "server1_vpc"
+    }
+  }
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "server1_subnet"
+  }
+}
+}
+
+
